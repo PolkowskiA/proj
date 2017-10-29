59f5b65af3eaef59e90d51bf484ea841ce510c62
PolkowskiA <arkadiusz.polkowski36@gmail.com>
2017-10-29 13:09:17 +0100

    Rebuild

--- db/migrate/20161130134956_create_projects.rb
+++ db/migrate/20161130134956_create_projects.rb
@@ -4,10 +4,6 @@ class CreateProjects < ActiveRecord::Migration[5.0]
       t.string :name
       t.timestamps
     end
-<<<<<<< HEAD
     add_column :projects, :user_id, :integer
-=======
-    add_index :projects, :user_id, :integer
->>>>>>> 0e80d7311c0b7b25923d2d5bf3fc97578f059b7e
   end
 end
